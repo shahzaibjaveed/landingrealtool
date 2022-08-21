f103c932e91758f48ccb3fe8a9e5a8775f012295
shahzaib.javeed <shahzaib.javeed@gmail.com>
2022-08-21 21:58:21 +0500

    Initial setup

---
+++ js/main.js
@@ -0,0 +1,69 @@
+(function ($) {
+  "use strict";
+
+  var fullHeight = function () {
+    $(".js-fullheight").css("height", $(window).height());
+    $(window).resize(function () {
+      $(".js-fullheight").css("height", $(window).height());
+    });
+  };
+  fullHeight();
+
+  var carousel = function () {
+    $(".services-carousel").owlCarousel({
+      loop: true,
+      autoplay: false,
+      margin: 30,
+      animateOut: "fadeOut",
+      animateIn: "fadeIn",
+      nav: true,
+      dots: false,
+      autoplayHoverPause: false,
+      items: 1,
+      navText: [
+        "<span class='ion-ios-arrow-back'></span>",
+        "<span class='ion-ios-arrow-forward'></span>",
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      },
+    });
+    $(".latestNews-carousel").owlCarousel({
+      loop: true,
+      autoplay: false,
+      margin: 30,
+      animateOut: "fadeOut",
+      animateIn: "fadeIn",
+      nav: true,
+      dots: false,
+      autoplayHoverPause: false,
+      items: 1,
+      navText: [
+        "<span class='ion-ios-arrow-back'></span>",
+        "<span class='ion-ios-arrow-forward'></span>",
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      },
+    });
+  };
+
+  carousel();
+  
+})(jQuery);
